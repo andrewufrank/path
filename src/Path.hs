337985d5e75e16738db36a44d25430f203b9bfc0
Andrew Frank <frank@geoinfo.tuwien.ac.at>
2018-02-27 17:30:42 +0100

    added isString

--- src/Path.hs
+++ src/Path.hs
@@ -7,6 +7,10 @@
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE FlexibleInstances  #-}
+--{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+--{-# LANGUAGE StandaloneDeriving #-}
+--{-# LANGUAGE FlexibleInstances #-}
+--{-# LANGUAGE OverloadedStrings   #-}
 
 #if defined(mingw32_HOST_OS)
 module Path(module Path.Windows) where
@@ -16,18 +20,30 @@ module Path(module Path.Posix) where
 import Path.Posix
 #endif
 
+import Data.String
+
+
+instance IsString (Path Abs File) where
+    fromString = read
+instance IsString (Path Abs Dir) where
+    fromString = read
+instance IsString (Path Rel File) where
+    fromString = read
+instance IsString (Path Rel Dir) where
+    fromString = read
+
 instance Read (Path Abs Dir) where
-        readsPrec i r =   maybe []  (\res -> [(res, rem)] ) $ parseAbsDir x  
-                where  [(x ::String , rem)] = readsPrec i r
+        readsPrec i r =   maybe []  (\res -> [(res, rem1)] ) $ parseAbsDir x
+                where  [(x ::String , rem1)] = readsPrec i r
 instance Read (Path Abs File) where
-        readsPrec i r =  maybe []  (\res -> [(res, rem)] ) $ parseAbsFile x  
-                where  [(x ::String , rem)] = readsPrec i r
+        readsPrec i r =  maybe []  (\res -> [(res, rem1)] ) $ parseAbsFile x
+                where  [(x ::String , rem1)] = readsPrec i r
 --                       mres = parseAbsFile x :: Maybe (Path Abs File)
 
 instance Read (Path Rel Dir) where
-        readsPrec i r =  maybe []  (\res -> [(res, rem)] ) $ parseRelDir x 
-                where  [(x ::String , rem)] = readsPrec i r
+        readsPrec i r =  maybe []  (\res -> [(res, rem1)] ) $ parseRelDir x
+                where  [(x ::String , rem1)] = readsPrec i r
 instance Read (Path Rel File) where
-        readsPrec i r =  maybe []  (\res -> [(res, rem)] ) $ parseRelFile x 
-                where  [(x ::String , rem)] = readsPrec i r
+        readsPrec i r =  maybe []  (\res -> [(res, rem1)] ) $ parseRelFile x
+                where  [(x ::String , rem1)] = readsPrec i r
 
